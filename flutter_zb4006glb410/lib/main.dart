5f311fdc2805086427909e96fdfbabae39971244
Stenio Eric <stenioeric@gmail.com>
2024-03-10 19:12:08 -0300

    Implementado fade in

--- flutter_zb4006glb410/lib/main.dart
+++ flutter_zb4006glb410/lib/main.dart
@@ -6,16 +6,11 @@ void main() {
 
       theme: ThemeData(
         primarySwatch: Colors.indigo,
-        textTheme: TextTheme(
-          
-        )
-
+        textTheme: TextTheme()
       ),
 
       home: Scaffold(
-
         appBar: AppBar(
-
           title: Text("Locações Stênio", 
           style: TextStyle(color: Color.fromARGB(255, 5, 5, 5),fontSize: 24)) 
 
@@ -28,7 +23,7 @@ void main() {
             children: [
 
               Text(
-                'O homi tá apenas começando...',
+                'AI CALICA...........',
                 style: TextStyle(
                   fontFamily: 'Montserrat',
                   fontSize: 16.0,
@@ -39,98 +34,71 @@ void main() {
               Text(
                 'AI CALICA...........',
                 style: TextStyle(
-                  fontFamily: 'Montserrat',
+                  fontFamily: 'Arial',
                   fontSize: 16.0,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-            
-              CircularProgressIndicator(),
-
-              Image.network(
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOccdrm73FT-ML9-kpBon_TwAWN5gzykePdg&usqp=CAU'
-              ),
-
+              
+              Center(
+                child: FadeInImage.assetNetwork(
+                  placeholder: 'assets/loading.gif',
+                  image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOccdrm73FT-ML9-kpBon_TwAWN5gzykePdg&usqp=CAU',
+                ),
+              )
             ]
-
           )
-
         ),
 
-        	
-      
+        bottomNavigationBar: BottomAppBar (
+          
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
 
-        bottomNavigationBar: Row (
+            children:[
 
-          children:[
-
-            Expanded (
-
-              child: IconButton(
-              
-                icon: Icon(Icons.home),        
-                tooltip: 'HOME',
-
-                onPressed: () {
-
-                  print('HOME pressionado!');
-
-                },
-              
+              Expanded (
+                child: IconButton(             
+                  icon: Icon(Icons.home),        
+                  tooltip: 'HOME',
+                  onPressed: () {
+                    print('HOME pressionado!');
+                  },              
+                ),
               ),
 
-            ),
-
-            Expanded (
-
-              child: ElevatedButton(
-              
-                onPressed: () {
-              
-                  print('Botão 1 pressionado!');
-              
-                },
-                child: Text('BOTÃO 1'),
+              Expanded (
+                child: ElevatedButton(             
+                  onPressed: () {             
+                    print('Botão 1 pressionado!');             
+                  },
+                  child: Text('BOTÃO 1'),
+                ),
               ),
 
-            ),
-
-            Expanded (
-
-              child: IconButton(
-              
-                icon: Icon(Icons.cell_tower),        
-                tooltip: 'cell_tower',
-
-                onPressed: () {
-
-                  print('CELL pressionado!');
-
-                },
-              
+              Expanded (
+                child: IconButton(
+                  icon: Icon(Icons.cell_tower),        
+                  tooltip: 'cell_tower',
+                  onPressed: () {
+                    print('CELL pressionado!');
+                  },
+                ),
               ),
 
-            ),
-
-            Expanded (
-
-              child: ElevatedButton(
-              
-                onPressed: () {
-              
-                  print('Botão 2 pressionado!');
-              
-                },
-                child: Text('BOTÃO 2'),
+              Expanded (
+                child: ElevatedButton(             
+                  onPressed: () {           
+                    print('Botão 2 pressionado!');             
+                  },
+                  child: Text('BOTÃO 2'),
+                ),
               ),
-
-            ),
-
-          ]
-      
-        )
-
-      ));
+            ]                 
+          ),
+        ),
+      ),
+    );
 
   runApp(app);
 
